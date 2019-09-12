8919ba57de91ec3c1dab0fca8882acb21933bb99
Per Rovegård <per@rovegard.se>
2019-09-12 21:11:59 +0200

    Make AsyncTest stateless

--- src/test/scala/intent/styles/AsyncTest.scala
+++ src/test/scala/intent/styles/AsyncTest.scala
@@ -3,13 +3,10 @@ package intent.styles
 import intent._
 import scala.concurrent.Future
 
-class AsyncTest extends TestSuite with Intent[Unit]:
+class AsyncTest extends TestSuite with IntentStateless:
 
-  "an async test" - :
+  "an async test" :
     "can use whenComplete" in :
-      _ =>
-        val f = Future { 21 * 2 }
-        whenComplete(f):
-          result => expect(result).toEqual(42)
-
-  def emptyState = ()
+      val f = Future { 21 * 2 }
+      whenComplete(f):
+        result => expect(result).toEqual(42)
