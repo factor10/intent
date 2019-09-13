4f5bbf612f3329f1fe8a9cbe4e7693dacd1dfcfd
Per Rovegård <per@rovegard.se>
2019-09-13 09:22:39 +0200

    Make ToEqualTest stateless

--- src/test/scala/intent/matchers/ToEqualTest.scala
+++ src/test/scala/intent/matchers/ToEqualTest.scala
@@ -1,42 +1,24 @@
 package intent.matchers
 
-import intent.{Intent, TestSuite}
+import intent.{IntentStateless, TestSuite}
 
-class ToEqualTest extends TestSuite with Intent[Unit]:
+class ToEqualTest extends TestSuite with IntentStateless:
   "toEqual" - :
     // TODO: Some sort of table driven tests would be a nice addition
     "for Boolean" - :
-      "true should equal true" in { _ =>
-        expect(true).toEqual(true)
-      }
+      "true should equal true" in expect(true).toEqual(true)
 
-      "true should *not* equal false" in { _ =>
-        expect(true).not.toEqual(false)
-      }
+      "true should *not* equal false" in expect(true).not.toEqual(false)
 
-      "false should equal false" in { _ =>
-        expect(false).toEqual(false)
-      }
+      "false should equal false" in expect(false).toEqual(false)
 
-      "false should *note* equal true" in { _ =>
-        expect(false).not.toEqual(true)
-      }
+      "false should *note* equal true" in expect(false).not.toEqual(true)
 
     "for String" - :
-      "<empty> should equal <empty>" in { _ =>
-        expect("").toEqual("")
-      }
+      "<empty> should equal <empty>" in expect("").toEqual("")
 
-      "<foo> should equal <foo>" in { _ =>
-        expect("foo").toEqual("foo")
-      }
+      "<foo> should equal <foo>" in expect("foo").toEqual("foo")
 
-      "<foo> should *not* equal <bar>" in { _ =>
-        expect("foo").not.toEqual("bar")
-      }
+      "<foo> should *not* equal <bar>" in expect("foo").not.toEqual("bar")
 
-      "<🤓> should equal <🤓>" in { _ =>
-        expect("🤓").toEqual("🤓")
-      }
-
-  def emptyState = ()
+      "<🤓> should equal <🤓>" in expect("🤓").toEqual("🤓")
