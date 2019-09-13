9c4310ca00760cb8167abbe686c33c07988fd1e5
Per Rovegård <per@rovegard.se>
2019-09-13 11:23:05 +0200

    Convert tests that are State[Unit] to Stateless

--- src/test/scala/intent/matchers/ToHaveLengthTest.scala
+++ src/test/scala/intent/matchers/ToHaveLengthTest.scala
@@ -1,23 +1,13 @@
 package intent.matchers
 
-import intent.{State, TestSuite}
+import intent.{Stateless, TestSuite}
 
-class ToHaveLengthTest extends TestSuite with State[Unit]:
-  "toHaveLength" - :
-    "empty list should have length 0" in { _ =>
-      expect(Seq()).toHaveLength(0)
-    }
+class ToHaveLengthTest extends TestSuite with Stateless:
+  "toHaveLength" :
+    "empty list should have length 0" in expect(Seq()).toHaveLength(0)
 
-    "Seq(1) should have length 1" in { _ =>
-      expect(Seq(1)).toHaveLength(1)
-    }
+    "Seq(1) should have length 1" in expect(Seq(1)).toHaveLength(1)
 
-    "Nested Seq(Seq(), Seq()) should have length 2" in { _ =>
-      expect(Seq(Seq(), Seq())).toHaveLength(2)
-    }
+    "Nested Seq(Seq(), Seq()) should have length 2" in expect(Seq(Seq(), Seq())).toHaveLength(2)
 
-    "Seq() should *not* have length 1" in { _ =>
-      expect(Seq()).not.toHaveLength(1)
-    }
-
-  def emptyState = ()
+    "Seq() should *not* have length 1" in expect(Seq()).not.toHaveLength(1)
--- src/test/scala/intent/suite/TestDiscoveryTest.scala
+++ src/test/scala/intent/suite/TestDiscoveryTest.scala
@@ -1,29 +1,21 @@
 package intent.suite
 
-import intent.{TestSuite, State}
+import intent.{TestSuite, Stateless}
 import intent.testdata._
 
-class TestDiscoveryTest extends TestSuite with State[Unit]:
-  "Test discovery" - :
-    "empty test suite" - :
+class TestDiscoveryTest extends TestSuite with Stateless:
+  "Test discovery" :
+    "empty test suite" :
       val suite = new EmtpyTestSuite()
 
-      "should have 0 tests" in { _ =>
-        expect(suite.allTestCases).toHaveLength(0)
-      }
+      "should have 0 tests" in expect(suite.allTestCases).toHaveLength(0)
 
-    "single level test suite" - :
+    "single level test suite" :
       val suite = new SingleLevelTestSuite()
 
-      "should have 1 tests" in { _ =>
-        expect(suite.allTestCases).toHaveLength(1)
-      }
+      "should have 1 tests" in expect(suite.allTestCases).toHaveLength(1)
 
-    "nested test suite" - :
+    "nested test suite" :
       val suite = new NestedTestsSuite()
 
-      "should have 3 tests" in { _ =>
-        expect(suite.allTestCases).toHaveLength(3)
-      }
-
-  def emptyState = ()
+      "should have 3 tests" in expect(suite.allTestCases).toHaveLength(3)
