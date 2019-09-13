735da49e20b7e38defcb0b51558e010195141820
Per Rovegård <per@rovegard.se>
2019-09-13 11:34:00 +0200

    Make ExpectTest stateless

--- src/test/scala/intent/matchers/ExpectTest.scala
+++ src/test/scala/intent/matchers/ExpectTest.scala
@@ -4,24 +4,18 @@ import intent._
 
 import scala.concurrent.Future
 
-case class MyOtherState()
+class ExpectTest extends TestSuite with Stateless:
+  "an expectation" :
+    "can be negated" in expect(1 + 2).not.toEqual(4)
 
-class ExpectTest extends TestSuite with State[MyOtherState]:
-  "an expectation" - :
-    "can be negated" in { _ =>
-      expect(1 + 2).not.toEqual(4)
-    }
-    "can check a Future" in { _ =>
+    "can check a Future" in:
       val f = Future { 1 + 2 }
       expect(f).toCompleteWith(3)
-    }
-    "can check a Seq" in { _ =>
+
+    "can check a Seq" in:
       val s = Seq(1, 2, 3)
       expect(s).toContain(2)
-    }
-    "can check a List" in { _ =>
+
+    "can check a List" in:
       val l = List(1, 2, 3)
       expect(l).not.toContain(4)
-    }
-
-  def emptyState = MyOtherState()
