7ad4f38e6d6991d3d618a2547508e98e49bff5fa
Per Rovegård <per@rovegard.se>
2019-09-19 13:35:46 +0200

    Add negation test for toCompleteWith

--- src/test/scala/intent/matchers/ToCompleteWithTest.scala
+++ src/test/scala/intent/matchers/ToCompleteWithTest.scala
@@ -7,3 +7,4 @@ class ToCompleteWithTest extends TestSuite with Stateless:
   "toCompleteWith" :
     "for successful Future" :
         "should be completed" in expect(Future.successful("foo")).toCompleteWith("foo")
+        "can be negated" in expect(Future.successful("foo")).not.toCompleteWith("bar")
