70a579b8e5d24f232c8989c9d2322d5ec4cebecc
Markus Eliasson <markus.eliasson@factor10.com>
2019-09-19 11:18:08 +0200

    No need for explicit return

--- src/test/scala/intent/helpers/Meta.scala
+++ src/test/scala/intent/helpers/Meta.scala
@@ -7,9 +7,9 @@ import scala.concurrent.{Future, ExecutionContext}
 trait Meta:
   self: TestLanguage with TestSupport =>
     def runExpectation(e: => Expectation, expected: String) given (ExecutionContext): Expectation =
-      return new Expectation :
+      new Expectation :
         def evaluate(): Future[ExpectationResult] =
           e.evaluate().flatMap:
             case TestFailed(s) => expect(s).toEqual(expected).evaluate()
-            case TestPassed()  => Future.successful(TestFailed("Expected a test failure")) // TODO: Describe the expect call when the macro stuff is in 
+            case TestPassed()  => Future.successful(TestFailed("Expected a test failure")) // TODO: Describe the expect call when the macro stuff is in
             case t: TestError  => Future.successful(t)
