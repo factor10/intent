b80057e701471d5388f40471fad39a7f452f625d
Per Rovegård <per@rovegard.se>
2019-09-30 16:34:07 +0200

    Add DelayedFuture

---
+++ src/main/scala/intent/util/futures.scala
@@ -0,0 +1,57 @@
+package intent.util
+
+import java.util.concurrent.atomic.AtomicBoolean
+import java.util.{Timer, TimerTask}
+
+import scala.concurrent.duration.Duration
+import scala.concurrent._
+import scala.util.Try
+
+trait DelayedFuture[T] extends Future[T] {
+  def cancel(): Unit
+}
+
+// From: http://stackoverflow.com/questions/16359849/scala-scheduledfuture
+object DelayedFuture :
+  private val timer = new Timer
+
+  private def makeTask[T](body: => T)(schedule: TimerTask => Unit) given (ctx: ExecutionContext): Future[T] =
+    val prom = Promise[T]()
+    schedule(
+      new TimerTask {
+        override def run() =
+          // IMPORTANT: The timer task just starts the execution on the passed
+          // ExecutionContext and is thus almost instantaneous (making it
+          // practical to use a single  Timer - hence a single background thread).
+          ctx.execute(() => prom.complete(Try(body)))
+      }
+    )
+    prom.future
+
+  def apply[T](duration: Duration)(body: => T) given (ctx: ExecutionContext): DelayedFuture[T] =
+    val isCancelled = new AtomicBoolean(false)
+    val f = makeTask({
+      if (!isCancelled.get()) body else null.asInstanceOf[T]
+    })(timer.schedule(_, duration.toMillis))
+    DelayedFutureImpl(f, () => isCancelled.set(true))
+
+  private class DelayedFutureImpl[T](val inner: Future[T], cancelFun: () => Unit) extends DelayedFuture[T] :
+    override def cancel(): Unit = cancelFun()
+
+    override def onComplete[U](f: (Try[T]) => U)(implicit executor: ExecutionContext): Unit = inner.onComplete(f)
+
+    override def isCompleted: Boolean = inner.isCompleted
+
+    override def value: Option[Try[T]] = inner.value
+
+    override def ready(atMost: Duration)(implicit permit: CanAwait): DelayedFutureImpl.this.type =
+      inner.ready(atMost)
+      this
+
+    override def result(atMost: Duration)(implicit permit: CanAwait): T = inner.result(atMost)
+
+    override def transform[S](f: Try[T] => Try[S])(implicit executor: ExecutionContext): Future[S] =
+      inner.transform(f)
+
+    override def transformWith[S](f: Try[T] => Future[S])(implicit executor: ExecutionContext): Future[S] =
+      inner.transformWith(f)
---
+++ src/test/scala/intent/util/DelayedFutureTest.scala
@@ -0,0 +1,77 @@
+package intent.util
+
+import intent._
+import intent.core.Expectation
+
+import java.util.concurrent.Executors
+
+import scala.concurrent.{Await, ExecutionContext, Promise}
+import scala.concurrent.duration._
+import scala.language.postfixOps
+import scala.util.{Success, Try}
+
+class DelayedFutureTestState :
+  val executorService = Executors.newFixedThreadPool(1)
+  given executionContext as ExecutionContext = ExecutionContext.fromExecutorService(executorService)
+
+  val executorThreadId =
+    // Determine the thread ID of the execution context, so we can compare with that in the tests.
+    val p = Promise[Long]()
+    executionContext.execute(() => p.success(Thread.currentThread().getId))
+    Await.result(p.future, 2 seconds)
+
+  def run(f: given (ExecutionContext) => Expectation): Expectation = f
+
+object DelayedFutureTestState:
+  val instance = DelayedFutureTestState()
+
+class DelayedFutureTest extends TestSuite with State[DelayedFutureTestState] :
+
+  "A DelayedFuture" using (DelayedFutureTestState.instance) to :
+
+    "should run the callback in the supplied execution context" in :
+      s =>
+        s.run :
+          val f = DelayedFuture(10 milliseconds)(Thread.currentThread().getId)
+          whenComplete(f) :
+            tid => expect(tid).toEqual(s.executorThreadId)
+
+    "should be cancellable" in:
+      s =>
+        s.run:
+          var hasRun = false
+          val f = DelayedFuture(100 milliseconds) {
+            hasRun = true
+          }
+          f.cancel()
+          whenComplete(f) :
+            _ => expect(hasRun).toEqual(false)
+
+    "should have a default result after being cancelled" in:
+      s =>
+        s.run:
+          val f = DelayedFuture(100 milliseconds) { 42 }
+          f.cancel()
+          whenComplete(f):
+            value => expect(value).toEqual(0)
+
+    "should be chainable" in:
+      s =>
+        s.run:
+          val f = DelayedFuture(10 milliseconds) { 21 }.map(_ * 2)
+          whenComplete(f):
+            result => expect(result).toEqual(42)
+
+    "should be awaitable with result" in:
+      s =>
+        s.run:
+          val f = DelayedFuture(10 milliseconds) { 42 }
+          val result = Await.result(f, 100 milliseconds)
+          expect(result).toEqual(42)
+
+    "should be awaitable without result" in:
+      s =>
+        s.run:
+          val f = DelayedFuture(10 milliseconds) { 42 }
+          val f2 = Await.ready(f, 100 milliseconds)
+          expect[Option[Try[Int]]](f2.value).toEqual(Some(Success(42)))
