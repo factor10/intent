0b3a532c3d06291fe480a20668849e8f5c018039
Per Rovegård <per@rovegard.se>
2019-10-01 11:21:21 +0200

    ToContainTest: Fix indentation

--- src/test/scala/intent/matchers/ToContainTest.scala
+++ src/test/scala/intent/matchers/ToContainTest.scala
@@ -5,13 +5,13 @@ import intent.{Stateless, TestSuite}
 class ToContainTest extends TestSuite with Stateless:
   "toContain" :
     "a list of Int" :
-        "should contain element" in expect(Seq(1, 2, 3)).toContain(2)
-        "should **not** contain missing element" in expect(Seq(1, 2, 3)).not.toContain(4)
+      "should contain element" in expect(Seq(1, 2, 3)).toContain(2)
+      "should **not** contain missing element" in expect(Seq(1, 2, 3)).not.toContain(4)
 
     "a list of String" :
-        "should contain element" in expect(Seq("one", "two", "three")).toContain("two")
-        "should **not** contain missing element" in expect(Seq("one", "two", "three")).not.toContain("four")
+      "should contain element" in expect(Seq("one", "two", "three")).toContain("two")
+      "should **not** contain missing element" in expect(Seq("one", "two", "three")).not.toContain("four")
 
     "a list of Boolean" :
-        "should contain element" in expect(Seq(true, false)).toContain(false)
-        "should **not** contain missing element" in expect(Seq(true)).not.toContain(false)
+      "should contain element" in expect(Seq(true, false)).toContain(false)
+      "should **not** contain missing element" in expect(Seq(true)).not.toContain(false)
