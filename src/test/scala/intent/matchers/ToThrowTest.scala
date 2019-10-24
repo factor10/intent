4bea77471fcf4101925d7caf8850b12dfa88ab2b
Per Rovegård <per@rovegard.se>
2019-10-24 12:54:30 +0200

    Test that toThrow handles NPE

--- src/test/scala/intent/matchers/ToThrowTest.scala
+++ src/test/scala/intent/matchers/ToThrowTest.scala
@@ -10,6 +10,8 @@ class ToThrowTest extends TestSuite with Stateless with Meta
 
       "should find negated match" in expect(throwIllegalState).not.toThrow[IllegalArgumentException]()
 
+      "should handle NPE" in expect(throwNPE).toThrow[NullPointerException]()
+
       "should detect wrong exception" in:
         runExpectation(expect(throwIllegalState).toThrow[IllegalArgumentException](),
           "Expected the code to throw java.lang.IllegalArgumentException, but it threw java.lang.IllegalStateException")
@@ -39,6 +41,7 @@ class ToThrowTest extends TestSuite with Stateless with Meta
     "with exception type and expected message":
       "should find match" in expect(throwIllegalArg).toThrow[IllegalArgumentException]("arg error")
       "should find negated match" in expect(throwIllegalArg).not.toThrow[IllegalArgumentException]("wrong text")
+      "should handle NPE" in expect(throwNPE).toThrow[NullPointerException](null.asInstanceOf[String])
 
     "with exception type and expected RegExp message":
       "should find match" in expect(throwIllegalArg).toThrow[IllegalArgumentException]("er+".r)
@@ -46,4 +49,5 @@ class ToThrowTest extends TestSuite with Stateless with Meta
     
   def throwIllegalArg = throw new IllegalArgumentException("arg error")
   def throwIllegalState = throw new IllegalStateException("state error")
+  def throwNPE = throw new NullPointerException()
   def dontThrow = "tada"
